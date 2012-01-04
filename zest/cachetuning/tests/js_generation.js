a7b011c0fb1f3649b1bd103c46a189d7e31cb244
Vincent Pretre <v.pretre@zestsoftware.nl>
2012-01-04 15:28:53 +0100

    Added tests for the JS file generation

--- zest/cachetuning/tests/js_generation.js
+++
@@ -1,9 +0,0 @@
-JS library generation
-=====================
-
-The Javascript file used by our tool content will depend on the settings of the user.
-
-    >>> self.login_as_manager()
-    >>> self.addProduct('zest.cachetuning')
-
-    >>> self.browser.getLink('Site Setup').click()
